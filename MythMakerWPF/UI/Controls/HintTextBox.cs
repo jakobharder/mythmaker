73902db5c18982d3ea9026fcd5ccafe04e382459
Jakob Harder <jharder@live.com>
2018-10-29 20:51:24 +0100

    add watermark hint to item bonus text (D10, FD)

---
+++ MythMakerWPF/UI/Controls/HintTextBox.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * based on the work by Miňo
+ * https://code.msdn.microsoft.com/TextBox-with-null-text-hint-0b384543
+ * Apache License Version 2.0
+ */
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MythMaker.UI.Controls
+{
+    public class HintTextBox : TextBox
+    {
+        static HintTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HintTextBox),
+                new FrameworkPropertyMetadata(typeof(HintTextBox)));
+        }
+
+        public static readonly DependencyProperty NullTextProperty =
+             DependencyProperty.Register("HintText", typeof(string),
+                typeof(HintTextBox), new FrameworkPropertyMetadata("Zadať"));
+
+        public string HintText
+        {
+            get { return (string)GetValue(NullTextProperty); }
+            set { SetValue(NullTextProperty, value); }
+        }
+    }
+}
